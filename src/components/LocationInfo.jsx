4cc67dd644d4251f9086646e8d654248ca9aa1ec
Carlos <josesolrac13@gmail.com>
2022-10-04 16:53:21 -0500

    mejora de fuente y estilos

--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import locationInfo from "./styles/locationInfo.css"
 
 const LocationInfo = ({location}) => {
 
   return (
-    <article>
-      <h2>{location?.name}</h2>
-      <ul>
-        <li><span>Tipe: </span>{location?.type}</li>
-        <li><span>Dimension: </span>{location?.dimension}</li>
-        <li><span>Population: </span>{location?.residents.length}</li>
+    <article className='planet__info'>
+      <h2 className='planet__title'>{location?.name}</h2>
+      <ul className='planet__description'>
+        <li className='planet__atribute'><span>Tipe: </span>{location?.type}</li>
+        <li className='planet__atribute'><span>Dimension: </span>{location?.dimension}</li>
+        <li className='planet__atribute'><span>Population: </span>{location?.residents.length}</li>
       </ul>
 
 
